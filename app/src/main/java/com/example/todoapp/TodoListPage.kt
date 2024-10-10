8b22f1d83fd6986f368138ea0df53d46ae6b6220
Noah <ninjayek@gmail.com>
2024-10-10 17:22:07 +0800

    Added alignment to the row

--- app/src/main/java/com/example/todoapp/TodoListPage.kt
+++ app/src/main/java/com/example/todoapp/TodoListPage.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -50,7 +51,8 @@ fun TodoItem(item: Todo) {
             .padding(8.dp)
             .clip(RoundedCornerShape(16.dp))
             .background(MaterialTheme.colorScheme.primary)
-            .padding(16.dp)
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
         Column(
             modifier = Modifier.weight(1f)

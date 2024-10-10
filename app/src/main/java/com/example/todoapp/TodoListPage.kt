fe87ca65a1be4acab5b481235fb745a45ddc08db
Noah <ninjayek@gmail.com>
2024-10-10 17:46:40 +0800

    Added logic function into TodoManager and TodoViewModel

--- app/src/main/java/com/example/todoapp/TodoListPage.kt
+++ app/src/main/java/com/example/todoapp/TodoListPage.kt
@@ -1,6 +1,7 @@
 package com.example.todoapp
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,11 +10,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -27,12 +34,28 @@ import java.util.Locale
 @Composable
 fun TodoListPage(){
     val todoList = getFakeTodo()
+    var inputText by remember {
+        mutableStateOf("")
+    }
 
     Column (
         modifier = Modifier
             .fillMaxSize()
             .padding(8.dp)
     ) {
+        Row (
+            modifier = Modifier.fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            OutlinedTextField(value = inputText , onValueChange = {
+                inputText = it
+            } )
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Add")
+            }
+        }
+
         LazyColumn(
             content = {
                 itemsIndexed(todoList){index: Int, item: Todo ->
